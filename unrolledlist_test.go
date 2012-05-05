709aab3f31671dc2048a7b338d386539e54efc6f
Ric Szopa <ryszard.szopa@gmail.com>
2012-05-05 16:29:34 -0700

    initial import

---
+++ unrolledlist_test.go
@@ -0,0 +1,242 @@
+package unrolledlist
+
+import (
+	"testing"
+	"fmt"
+)
+
+func (l UnrolledList) nodeLength() int {
+	if l.next == nil {
+		return 1
+	}
+	return 1 + l.next.nodeLength()
+}
+
+func (l UnrolledList) repr() string {
+	if l.next == nil {
+		return fmt.Sprintf("%v", l.elements)
+	}
+	return fmt.Sprintf("%v %v", l.elements, l.next.repr())
+}
+
+func TestAppendOneNode(t *testing.T) {
+	list := New(10)
+	for i := 0; i < 5; i++ {
+		list.Append(i)
+	}
+
+	for i := 0; i < 5; i++ {
+		if value := list.Get(i); value != i {
+			t.Errorf("Wrong value for index %d: %v", i, value)
+		}
+	}
+
+}
+
+func TestMoreNodes(t *testing.T) {
+	list := New(3)
+	for i := 0; i < 5; i++ {
+		list.Append(i)
+	}
+	for i := 0; i < 5; i++ {
+		if value := list.Get(i); value != i {
+			t.Errorf("Wrong value for index %d: %v", i, value)
+		}
+	}
+
+}
+
+func ListLike(t *testing.T, list *UnrolledList, values ...int) {
+	wasError := false
+	for i, wanted := range values {
+		if value := list.Get(i); value != wanted {
+			wasError = true
+			t.Errorf("Wrong value for index %d: %v (should be %v)", i, value, wanted)
+		}
+	}
+	if wasError {
+		for l := list; l != nil; l = l.next {
+			t.Errorf("|", l.elements)
+		}
+	}
+}
+
+func TestDowncaseInsert(t *testing.T) {
+	sl := make([]interface{}, 3, 3)
+	sl[0] = 0
+	sl[1] = 2
+	sl = insert(sl, 1, 1)
+	if sl[0] != 0 || sl[1] != 1 || sl[2] != 2 {
+		t.Errorf("slice is not ordered: %v.", sl)
+	}
+}
+
+func TestInsertOneNode(t *testing.T) {
+	list := New(5)
+	list.Append(0)
+	list.Append(2)
+	list.Insert(1, 1)
+	ListLike(t, list, 0, 1, 2)
+
+}
+
+func TestInsertIntoLastNode(t *testing.T) {
+	list := New(4)
+
+	for i := 0; i < 7; i++ {
+		list.Append(i)
+	}
+	list.Insert(6, 1000)
+	ListLike(t, list, 0, 1, 2, 3, 4, 5, 1000, 6)
+}
+
+func TestInsertIntoMiddleNode(t *testing.T) {
+	list := New(3)
+
+	for i := 0; i < 7; i++ {
+		list.Append(i)
+	}
+	list.Insert(3, 1000)
+	ListLike(t, list, 0, 1, 2, 1000, 3, 4, 5, 6)
+}
+
+func TestInsertOutOfBounds(t *testing.T) {
+	list := New(3)
+	if err := list.Insert(100, 1); err == nil {
+		t.Error("Out of bound insert didn't return an error.")
+	}
+}
+
+func TestIteration(t *testing.T) {
+	list := New(3)
+	for i := 0; i < 10; i++ {
+		list.Append(i)
+	}
+	wanted := 0
+	for el := range list.Iter() {
+		if wanted != el {
+			t.Errorf("Wanted %v, got %v.", wanted, el)
+		}
+		wanted++
+	}
+	if wanted != 10 {
+		t.Errorf("Iter was supposed to yield 10 times, yielded %d.", wanted)
+	}
+}
+
+func TestSliceDelete(t *testing.T) {
+	arr := []interface{}{0, 1, 2}
+	el, arr := sliceDelete(arr, 1)
+	if el != 1 {
+		t.Errorf("Got %v, expected 1 (%v)", el, arr)
+	}
+	if arr[0] != 0 || arr[1] != 2 {
+		t.Errorf("Got %v, expected [0, 2]", arr)
+	}
+}
+
+func TestPopSimple(t *testing.T) {
+	list := New(5)
+	list.Append(0)
+	list.Append(1)
+	list.Append(2)
+
+	nodeLength := list.nodeLength()
+
+	if el := list.Pop(1); el != 1 {
+		t.Errorf("Got %v, expected 1", el)
+	}
+	ListLike(t, list, 0, 2)
+	if newNodeLength := list.nodeLength(); newNodeLength != nodeLength {
+		t.Errorf("Node length has changed even though it shouldn't have: %v, not %v", newNodeLength, nodeLength)
+	}
+}
+
+func TestPopNotInFirstNode(t *testing.T) {
+	list := New(3)
+	for i := 0; i < 10; i++ {
+		list.Append(i)
+	}
+
+	nodeLength := list.nodeLength()
+
+	if el := list.Pop(5); el != 5 {
+		t.Errorf("Got %v, expected 1", el)
+	}
+	ListLike(t, list, 0, 1, 2, 3, 4, 6, 7, 8, 9)
+	if newNodeLength := list.nodeLength(); newNodeLength != nodeLength {
+		t.Errorf("Node length has changed even though it shouldn't have: %v, not %v", newNodeLength, nodeLength)
+	}
+
+}
+
+func TestPopNodeMoveElementsFromAdjacent(t *testing.T) {
+	list := New(4)
+	for i := 0; i < 8; i++ {
+		list.Append(i)
+	}
+	// [0, 1, 2, 3], [4, 5, 6, 7]
+	list.Pop(0)
+	list.Pop(0) // after: [2, 3], [4, 5, 6, 7]
+
+	list.Pop(0) // after: [3, 4] [5, 6, 7]
+	if l1, l2 := len(list.elements), len(list.next.elements); l1 != 2 || l2 != 3 {
+		t.Errorf("The layout of the elements is wrong. It should be ([3 4] [5 6 7]), (%v) found.", list.repr())
+	}
+	ListLike(t, list, 3, 4, 5, 6, 7)
+
+}
+
+func TestPopNodeMoveElementsWithMerge(t *testing.T) {
+	list := New(4)
+	for i := 0; i < 12; i++ {
+		list.Append(i)
+	}
+	if nl := list.nodeLength(); nl != 3 {
+		t.Errorf("The node length isn't 3: %v", nl)
+	}
+
+	for i := 0; i < 4; i++ {
+		list.Pop(0)
+	}
+	if nl, l1, l2 := list.nodeLength(), len(list.elements), len(list.next.elements); nl != 2 || l1 != 4 || l2 != 4 {
+		t.Errorf("The layout of the elements is wrong. It should be ([4 5 6 7] [8 9 10 11]), (%v) found.", list.repr())
+	}
+
+	ListLike(t, list, 4, 5, 6, 7, 8, 9, 10, 11)
+
+}
+
+// TODO: Pop for last cell
+
+func TestOutOfBounds(t *testing.T) {
+	list := New(3)
+	for i := 0; i < 10; i++ {
+		list.Append(i)
+	}
+
+	if el := list.Pop(100); el != nil {
+		t.Errorf("Out of bound element should be nil, not %v", el)
+	}
+
+	list = New(3)
+	list.Append(0)
+	if el := list.Pop(1); el != nil {
+		t.Errorf("Out of bound element should be nil, not %v", el)
+	}
+
+}
+
+func TestLength(t *testing.T) {
+	list := New(3)
+	if l := list.Length(); l != 0 {
+		t.Errorf("An empty list should have length 0, not %v.", l)
+	}
+	for i := 0; i < 10; i++ {
+		list.Append(i)
+	}
+	if l := list.Length(); l != 10 {
+
+	}
+
+}
